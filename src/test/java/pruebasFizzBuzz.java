e59308481f0a40fc1a626497dc0dc16ce54b6500
SergioArispeJulio <sergio_arispe@hotmail.com>
2013-09-18 22:30:12 -0400

    Prueba

---
+++ src/test/java/pruebasFizzBuzz.java
@@ -0,0 +1,55 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class pruebasFizzBuzz {
+
+	Clase prueba;
+	
+	public pruebasFizzBuzz(){
+		prueba = new Clase();
+	}
+	
+	@Test
+	public void truemultiplo3() {
+		assertTrue(prueba.multiplo3(3));
+	}
+	
+	@Test
+	public void truemultiplo5() {
+		assertTrue(prueba.multiplo5(5));
+	}
+	
+	@Test
+	public void falsemultiplo3() {
+		assertFalse(prueba.multiplo3(2));
+	}
+	
+	@Test
+	public void falsemultiplo5() {
+		assertFalse(prueba.multiplo5(3));
+	}
+
+	@Test
+	public void devolverfizz(){
+		assertEquals("Fizz", prueba.resultado(3));
+	}
+	
+	@Test
+	public void devolverbuzz(){
+		assertEquals("Buzz", prueba.resultado(5));
+	}
+	
+	@Test
+	public void devolverfizzbuzz(){
+		assertEquals("FizzBuzz", prueba.resultado(15));
+	}
+	
+	@Test
+	public void devolvernormal(){
+		assertEquals(7, 7);
+	}
+	
+
+}
